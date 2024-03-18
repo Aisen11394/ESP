4497ca83aba1eed1e4afe3a40a946c5d6f2f1577
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-03-18 12:06:57 +0900

    Update ON.lua

--- ON.lua
+++ ON.lua
@@ -1,5 +1,5 @@
 local IsDisabled = true
-local IsTeamCheckEnabled = true 
+local IsTeamCheckEnabled = false 
 
 game:GetService('RunService').RenderStepped:Connect(function()
     if IsDisabled then
@@ -12,10 +12,13 @@ game:GetService('RunService').RenderStepped:Connect(function()
             if player ~= localPlayer and (not IsTeamCheckEnabled or player.Team ~= localPlayerTeam) then
                 local humanoidRootPart = player.Character and player.Character:FindFirstChild("HumanoidRootPart")
                 if humanoidRootPart then
-                    humanoidRootPart.Size = Vector3.new(6, 5, 4)
-                    humanoidRootPart.Transparency = 0.45
+                    humanoidRootPart.Size = Vector3.new(6, 7, 4)
+                    humanoidRootPart.Transparency = 0.42
                     humanoidRootPart.BrickColor = BrickColor.new("Really red")
                     humanoidRootPart.Material = Enum.Material.Neon
+                    humanoidRootPart.CanCollide = false
+                end
+            end
         end
     end
 end)
